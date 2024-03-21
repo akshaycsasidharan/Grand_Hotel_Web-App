994d4cb4bb47965b0cc2405779515ee95e9fea83
akshay c s <akshaycs0480@gmail.com>
2024-03-21 21:56:01 +0530

    jwt continues

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({ secret: 'Key', cookie: { maxAge: 600000 } }))
+// app.use(session({ secret: 'Key', cookie: { maxAge: 600000 } }))
 
 app.use((req, res, next) => {
   res.set('cache-control', 'no-store')
